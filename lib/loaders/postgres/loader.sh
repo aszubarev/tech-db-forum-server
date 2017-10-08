3e18963c075447cea48db2de1cc005bb75596838
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-08 19:11:54 +0300

    WIP: fix order start service by sleep

--- lib/loaders/postgres/loader.sh
+++ lib/loaders/postgres/loader.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "sleep"
+sleep 5
+echo "not sleep"
+
 echo "try connect to another container and fill database from *.psql files"
 psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/users.psql
 
