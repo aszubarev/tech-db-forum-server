e51b2336d6f6282d8f7a6a1e57a726e7ba2ab507
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-08 19:31:53 +0300

    fix loader by wait_postgres function

--- lib/loaders/postgres/loader.sh
+++ lib/loaders/postgres/loader.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "sleep"
-sleep 5
-echo "not sleep"
+function wait_postgres {
+    echo "Waiting postgres to run on postgres 5432..."
+
+    while ! psql -h postgres -p 5432 -U my_user -d my_db -c "SELECT datname FROM pg_database LIMIT 1">&/dev/null; do
+      echo "sleep 1"
+      sleep 1
+    done
+
+    echo "Postgres launched"
+}
+
+wait_postgres
 
 echo "try connect to another container and fill database from *.psql files"
 psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/users.psql
