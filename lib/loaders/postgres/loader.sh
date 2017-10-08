23d269f35228c32b5bfcf47a88fe1f1594b7c122
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-08 22:04:01 +0300

    create table threads, fix schema

--- lib/loaders/postgres/loader.sh
+++ lib/loaders/postgres/loader.sh
@@ -21,4 +21,5 @@ echo "[TRY] connect to another container and fill database from *.psql files"
 psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/users.psql;
 psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/vote.psql;
 psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/forums.psql;
+psql -h postgres -p 5432 -U my_user -d my_db < /tmp/database/postgres/tables/threads.psql;
 
