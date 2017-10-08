8ec74597b093a7ab18cd43a70929ca19c7fb35c9
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-08 19:36:11 +0300

    remove echo

--- lib/loaders/postgres/loader.sh
+++ lib/loaders/postgres/loader.sh
@@ -3,9 +3,9 @@
 function wait_postgres {
     echo "Waiting postgres to run on postgres 5432..."
 
-    while ! psql -h postgres -p 5432 -U my_user -d my_db -c "SELECT datname FROM pg_database LIMIT 1">&/dev/null; do
-      echo "sleep 1"
-      sleep 1
+    while ! psql -h postgres -p 5432 -U my_user -d my_db -c "SELECT datname FROM pg_database LIMIT 1">&/dev/null;
+    do
+      sleep 0.1
     done
 
     echo "Postgres launched"
