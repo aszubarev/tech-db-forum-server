e36b5c7368317c6dfb13b1757a4f7f43d882d09e
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-18 03:59:26 +0300

    complete some tests

--- src/api/tech_forum_api/persistence/repositories/post_repository.py
+++ src/api/tech_forum_api/persistence/repositories/post_repository.py
@@ -47,33 +47,13 @@ class PostRepository(Repository[PostDTO]):
             # elif limit is None and since is None and desc is None:
             #     data = self._context.callproc('get_posts_for_thread_flat', [thread_id])
 
-        # TREE SORT
+        # PARENT_TREE SORT
         elif sort == 'parent_tree':
             if limit is not None and since is None and desc is None:
-                data = self._context.callproc('get_posts_for_thread_limit', [thread_id, limit])
-                parents = create_many(PostDTO, data)
-                entities = []
+                return self._get_posts_for_thread_limit(thread_id=thread_id, limit=limit)
 
-                for parent in parents:
-
-                    data.clear()
-                    data = self._context.callproc('get_post_by_parent_id', [parent.uid])
-                    children = create_many(PostDTO, data)
-                    if len(children) == 0:
-                        continue
-
-                    logging.info(f"===================================================================================")
-                    logging.info(f"[PostRepository] parent: uid = {parent.uid}; parent_id = {parent.parent_id}")
-                    logging.info(f"[PostRepository] numb children = {len(children)}")
-                    entities.append(parent)
-                    for child in children:
-                        logging.info(f"[PostRepository] child: uid = {child.uid}; parent_id = {child.parent_id}")
-                        entities.append(child)
-
-                logging.info(f"===================================================================================")
-                logging.info(f"numb parents = {len(parents)}; numb entities = {len(entities)}")
-
-                return entities
+            if limit is not None and since is None and desc is not None:
+                return self._get_posts_for_thread_desc_limit(thread_id=thread_id, desc=desc, limit=limit)
 
             # elif limit is None and since is None and desc is None:
             #     data = self._context.callproc('get_posts_for_thread_flat', [thread_id])
@@ -113,6 +93,63 @@ class PostRepository(Repository[PostDTO]):
 
     def delete(self, uid: int) -> None:
         raise NotImplementedError
+
+    def _get_posts_for_thread_limit(self, thread_id, limit: int) -> List[PostDTO]:
+        data = self._context.callproc('get_posts_for_thread_limit', [thread_id, limit])
+        parents = create_many(PostDTO, data)
+        entities = []
+
+        for parent in parents:
+
+            data.clear()
+            data = self._context.callproc('get_post_by_parent_id', [parent.uid])
+            children = create_many(PostDTO, data)
+            if len(children) == 0:
+                continue
+
+            logging.info(f"===================================================================================")
+            logging.info(f"[PostRepository] parent: uid = {parent.uid}; parent_id = {parent.parent_id}")
+            logging.info(f"[PostRepository] numb children = {len(children)}")
+            entities.append(parent)
+            for child in children:
+                logging.info(f"[PostRepository] child: uid = {child.uid}; parent_id = {child.parent_id}")
+                entities.append(child)
+
+        logging.info(f"===================================================================================")
+        logging.info(f"numb parents = {len(parents)}; numb entities = {len(entities)}")
+
+        return entities
+
+    def _get_posts_for_thread_desc_limit(self, thread_id: int, desc: bool, limit: int) -> List[PostDTO]:
+
+        # if desc is True:
+        #     return self._get_posts_for_thread_limit(thread_id, limit)
+        #
+        # else:
+        data = self._context.callproc('get_posts_for_thread_desc_limit', [thread_id, desc, limit])
+        parents = create_many(PostDTO, data)
+        entities = []
+
+        for parent in parents:
+
+            data.clear()
+            data = self._context.callproc('get_post_by_parent_id', [parent.uid])
+            children = create_many(PostDTO, data)
+            if len(children) == 0:
+                continue
+
+            logging.info(f"===================================================================================")
+            logging.info(f"[PostRepository] parent: uid = {parent.uid}; parent_id = {parent.parent_id}")
+            logging.info(f"[PostRepository] numb children = {len(children)}")
+            entities.append(parent)
+            for child in children:
+                logging.info(f"[PostRepository] child: uid = {child.uid}; parent_id = {child.parent_id}")
+                entities.append(child)
+
+        logging.info(f"===================================================================================")
+        logging.info(f"numb parents = {len(parents)}; numb entities = {len(entities)}")
+
+        return entities
 
     def _update_path(self, uid: int, path: List[int]):
         self._context.callproc('update_post_path_by_id', [uid, path])
