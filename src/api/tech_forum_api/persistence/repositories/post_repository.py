8e4e91b2500c44c9cc5cb5a468560a60ab69e26e
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-21 18:30:58 +0300

    remove some methods

--- src/api/tech_forum_api/persistence/repositories/post_repository.py
+++ src/api/tech_forum_api/persistence/repositories/post_repository.py
@@ -113,6 +113,7 @@ class PostRepository(Repository[PostDTO]):
     def get_all(self) -> List[PostDTO]:
         raise NotImplementedError
 
+    # TODO refactor this shit
     def add(self, entity: PostDTO) -> Optional[PostDTO]:
         data = self._context.callproc('add_post', [entity.thread_id, entity.forum_id, entity.user_id,
                                                    entity.parent_id, entity.message, entity.created, False])
@@ -139,95 +140,6 @@ class PostRepository(Repository[PostDTO]):
 
     def delete(self, uid: int) -> None:
         raise NotImplementedError
-
-    def _get_parent_posts_for_thread(self, thread_id, desc=None, limit=None, since=None) -> List[PostDTO]:
-
-        data = None
-
-        if desc is None and limit is not None and since is None:
-            data = self._context.callproc('get_posts_for_thread_limit', [thread_id, limit])
-
-        elif desc is not None and limit is not None and since is None:
-            data = self._context.callproc('get_posts_for_thread_desc_limit', [thread_id, desc, limit])
-
-        elif desc is None and limit is not None and since is not None:
-            data = self._context.callproc('get_posts_for_thread_limit_since', [thread_id, limit, since])
-
-        return create_many(PostDTO, data)
-
-    def _get_posts_for_thread_limit(self, thread_id, limit: int) -> List[PostDTO]:
-        # parents = self._get_parent_posts_for_thread(thread_id=thread_id, desc=None, limit=limit, since=None)
-        data = self._context.callproc('get_posts_for_thread_limit', [thread_id, limit])
-        parents = create_many(PostDTO, data)
-
-        entities = []
-        for parent in parents:
-
-            data.clear()
-            data = self._context.callproc('get_post_by_parent_id', [parent.uid])
-            children = create_many(PostDTO, data)
-
-            logging.info(f"===================================================================================")
-            logging.info(f"[PostRepository] parent: uid = {parent.uid}; parent_id = {parent.parent_id}")
-            logging.info(f"[PostRepository] numb children = {len(children)}")
-
-            if len(children) == 0:
-                continue
-
-            entities.append(parent)
-            for child in children:
-                logging.info(f"[PostRepository] child: uid = {child.uid}; parent_id = {child.parent_id}")
-                entities.append(child)
-
-        logging.info(f"===================================================================================")
-        logging.info(f"numb entities = {len(entities)}")
-
-        return entities
-
-    def _get_posts_for_thread_desc_limit(self, thread_id: int, desc: bool, limit: int) -> List[PostDTO]:
-
-        if desc == 'false':
-            return self._get_posts_for_thread_limit(thread_id=thread_id, limit=limit)
-
-        else:
-            data = self._context.callproc('get_posts_for_thread_desc_limit', [thread_id, desc, limit])
-            parents = create_many(PostDTO, data)
-            entities = []
-
-            for parent in parents:
-
-                data.clear()
-                data = self._context.callproc('get_post_by_parent_id', [parent.uid])
-                children = create_many(PostDTO, data)
-                if len(children) == 0:
-                    continue
-
-                for child in reversed(children):
-                    entities.append(child)
-
-                entities.append(parent)
-
-            return entities
-
-    def _get_posts_for_thread_parent_tree_limit_since(self, thread_id: int, limit: int, since: int):
-        # parents = self._get_parent_posts_for_thread(thread_id=thread_id, desc=None, limit=limit, since=since)
-        data = self._context.callproc('get_posts_for_thread_limit_since', [thread_id, limit, since])
-        parents = create_many(PostDTO, data)
-        entities = []
-        data = None
-        for parent in parents:
-
-            data.clear()
-            data = self._context.callproc('get_post_by_parent_id', [parent.uid])
-            children = create_many(PostDTO, data)
-            if len(children) == 0:
-                continue
-
-            entities.append(parent)
-            for child in children:
-                entities.append(child)
-
-        return entities
 
     def _update_path(self, uid: int, path: List[int]):
         self._context.callproc('update_post_path_by_id', [uid, path])
