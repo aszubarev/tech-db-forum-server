703c9eebe24ec80117a42a5eaa401b2a0cd1cbee
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-26 18:15:10 +0300

    add user_nickname and forum_slug fields to PostDTO

--- src/api/forum/persistence/dto/post_dto.py
+++ src/api/forum/persistence/dto/post_dto.py
@@ -13,7 +13,9 @@ class PostDTO(Entity):
     def __init__(self, uid: int = None,
                  thread_id: int = None,
                  forum_id: int = None,
+                 forum_slug: str = None,
                  user_id: int = None,
+                 user_nickname: str = None,
                  parent_id: int = None,
                  message: str = None,
                  created: datetime = None,
@@ -23,7 +25,9 @@ class PostDTO(Entity):
 
         self._thread_id = thread_id
         self._forum_id = forum_id
+        self._forum_slug = forum_slug
         self._user_id = user_id
+        self._user_nickname = user_nickname
         self._parent_id = parent_id
         self._message = message
         self._created = created
@@ -39,8 +43,16 @@ class PostDTO(Entity):
         return self._forum_id
 
     @property
+    def forum_slug(self) -> str:
+        return self._forum_slug
+
+    @property
     def user_id(self) -> int:
         return self._user_id
+
+    @property
+    def user_nickname(self) -> str:
+        return self._user_nickname
 
     @property
     def parent_id(self) -> int:
