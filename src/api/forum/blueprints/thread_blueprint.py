73d4cd3d576580d770b4f5a41fcbbfeefc51423f
ZubAnt <ya.zubarevanton@yandex.ru>
2017-11-07 20:32:22 +0300

    [WIP] refactor ThreadBlueprint

--- src/api/forum/blueprints/thread_blueprint.py
+++ src/api/forum/blueprints/thread_blueprint.py
@@ -1,42 +1,30 @@
 import dateutil.parser
-from flask import Blueprint, abort, request, Response, json
+from flask import Blueprint, request, Response, json
 from injector import inject, singleton
 
 from apiutils import BaseBlueprint
-from apiutils.errors.bad_request_error import BadRequestError
+from forum.persistence.repositories.forum_repository import ForumRepository
 from forum.persistence.repositories.thread_repository import ThreadRepository
-
-from forum.serializers.thread_serializer import ThreadSerializer
-from forum.serializers.vote_serializer import VoteSerializer
-from sqlutils import NoDataFoundError, UniqueViolationError
-from forum.services.forum_service import ForumService
-from forum.services.thread_service import ThreadService
-from forum.services.user_service import UserService
+from forum.persistence.repositories.user_repository import UserRepository
+from sqlutils import UniqueViolationError
 
 
 @singleton
 class ThreadBlueprint(BaseBlueprint[ThreadRepository]):
 
     @inject
-    def __init__(self, repo: ThreadRepository, serializer: ThreadSerializer, vote_serializer: VoteSerializer,
-                 user_service: UserService, forum_service: ForumService) -> None:
+    def __init__(self, repo: ThreadRepository, user_repo: UserRepository, forum_repo: ForumRepository) -> None:
         super().__init__(repo)
-        self.__serializer = serializer
-        self._userService = user_service
-        self._forumService = forum_service
-        self._voteSerializer = vote_serializer
+        self._userRepo = user_repo
+        self._forumRepo = forum_repo
 
     @property
     def _name(self) -> str:
         return 'threads'
 
     @property
-    def _serializer(self) -> ThreadSerializer:
-        return self.__serializer
-
-    @property
-    def __service(self) -> ThreadService:
-        return self._repository
+    def __repo(self) -> ThreadRepository:
+        return self._repo
 
     def _create_blueprint(self) -> Blueprint:
         blueprint = Blueprint(self._name, __name__)
@@ -45,29 +33,37 @@ class ThreadBlueprint(BaseBlueprint[ThreadRepository]):
         def _add(slug: str):
             try:
                 body = request.json
-                author = self._userService.get_by_nickname(body.get('author'))
+                author = self._userRepo.get_by_nickname(body.get('author'))
                 if not author:
                     return self._return_error(f"Can't find author for thread by nickname = {body.get('author')}", 404)
 
-                forum = self._forumService.get_by_slug(slug)
+                forum = self._forumRepo.get_by_slug(slug)
                 if not forum:
                     return self._return_error(f"Can't find forum for thread by slug = {slug}", 404)
 
-                response = self.__service.add_soft(body=body,
-                                                   user_id=author['user_id'], user_nickname=author['nickname'],
-                                                   forum_id=forum['forum_id'], forum_slug=forum['slug'])
-
+                params = body
+                forum_id = forum['forum_id']
+                params.update({
+                    'user_id': author['user_id'],
+                    'user_nickname': author['nickname'],
+                    'forum_id': forum_id,
+                    'forum_slug': forum['slug'],
+                    'slug': None if body.get('slug') is None else body['slug'],
+                    'created': None if body.get('created') is None else dateutil.parser.parse(body['created'])
+                })
+                response = self.__repo.add(params)
+                self._forumRepo.increment_threads(uid=forum_id)
                 return Response(response=json.dumps(response), status=201, mimetype='application/json')
 
             except UniqueViolationError:
                 thread_slug = request.json['slug']
-                response = self._repository.get_by_slug(thread_slug)
+                response = self.__repo.get_by_slug(thread_slug)
                 return Response(response=json.dumps(response), status=409, mimetype='application/json')
 
         @blueprint.route('forum/<slug>/threads', methods=['GET'])
         def _get_threads_by_forum(slug: str):
 
-            forum = self._forumService.get_by_slug(slug)
+            forum = self._forumRepo.get_by_slug(slug)
             if forum is None:
                 return self._return_error(f"Can't get threads by forum slag = {slug}", 404)
 
@@ -77,96 +73,62 @@ class ThreadBlueprint(BaseBlueprint[ThreadRepository]):
             if since is not None:
                 since = dateutil.parser.parse(since)
 
-            threads = self.__service.get_for_forum(forum['forum_id'], since=since, limit=limit, desc=desc)
+            threads = self.__repo.get_for_forum(forum['forum_id'], since=since, limit=limit, desc=desc)
             return Response(response=json.dumps(threads), status=200, mimetype='application/json')
 
         @blueprint.route('thread/<slug_or_id>/details', methods=['GET'])
         def _details(slug_or_id: str):
 
-            thread = self.__service.get_by_slug_or_id(slug_or_id)
+            thread = self.__repo.get_by_slug_or_id(slug_or_id)
             if not thread:
                 return self._return_error(f"Can't get thread by forum slug_or_id = {slug_or_id}", 404)
             return Response(response=json.dumps(thread), status=200, mimetype='application/json')
 
         @blueprint.route('thread/<slug_or_id>/details', methods=['POST'])
         def _update(slug_or_id: str):
-            try:
 
-                data = request.json
+            body = request.json
 
-                # empty request
-                if not data:
-                    thread = self.__service.get_by_slug_or_id(slug_or_id)
-                    if not thread:
-                        return self._return_error(f"Can't get thread by slug_or_id = {slug_or_id}", 404)
-                    return Response(response=json.dumps(thread), status=200, mimetype='application/json')
-
-                try:
-
-                    cast_thread_id = int(slug_or_id)
-
-                    data.update({
-                        'id': cast_thread_id
-                    })
-
-                    entity = self._serializer.load(data)
-                    thread = self.__service.update_by_uid(entity)
-
-                except ValueError:
-                    thread_slug = slug_or_id
-
-                    data.update({
-                        'slug': thread_slug
-                    })
-
-                    entity = self._serializer.load(data)
-                    thread = self.__service.update_by_slug(entity)
-
+            # empty request
+            if not body:
+                thread = self.__repo.get_by_slug_or_id(slug_or_id)
                 if not thread:
-                    return self._return_error(f"Can't update thread by slug_or_id = {slug_or_id}", 404)
+                    return self._return_error(f"Can't get thread by slug_or_id = {slug_or_id}", 404)
+                return Response(response=json.dumps(thread), status=200, mimetype='application/json')
 
-                return self._return_one(thread, status=200)
+            thread = self.__repo.update_by_slug_or_id(slug_or_id=slug_or_id,
+                                                      msg=body.get('message'),
+                                                      title=body.get('title'))
 
-            except NoDataFoundError as exp:
-                return self._return_error(f"Can't update thread by forum slug_or_id = {slug_or_id}", 404)
+            if not thread:
+                return self._return_error(f"Can't update thread by slug_or_id = {slug_or_id}", 404)
 
-            except BadRequestError as exp:
-                return self._return_error(f"Bad request", 400)
+            return self._return_one(thread, status=200)
 
         @blueprint.route('thread/<slug_or_id>/vote', methods=['POST'])
         def _vote(slug_or_id: str):
-            try:
 
-                data = request.json
-                nickname = data.get('nickname')
-                voice = data.get('voice')
-                if not nickname or not voice:
-                    return self._return_error(f"[ThreadBlueprint._vote] Bad request", 400)
+            data = request.json
+            nickname = data.get('nickname')
+            voice = data.get('voice')
+            if not nickname or not voice:
+                return self._return_error(f"[ThreadBlueprint._vote] Bad request", 400)
 
-                thread = self._repository.get_by_slug_or_id(slug_or_id)
-                if not thread:
-                    return self._return_error(f"Can't find thread by slug_or_id = {slug_or_id}", 404)
+            thread = self.__repo.get_by_slug_or_id(slug_or_id)
+            if not thread:
+                return self._return_error(f"Can't find thread by slug_or_id = {slug_or_id}", 404)
 
-                user = self._userService.get_by_nickname(nickname)
-                if not user:
-                    return self._return_error(f"Can't find user by nickname = {nickname}", 404)
+            user = self._userRepo.get_by_nickname(nickname)
+            if not user:
+                return self._return_error(f"Can't find user by nickname = {nickname}", 404)
 
-                data.update({
-                    'thread_id': thread['id'],
-                    'user_id': user['user_id'],
-                })
+            votes = self.__repo.vote(user_id=user['user_id'], thread_id=thread['id'], voice=voice)
+            if votes is None:
+                return self._return_error(f"[ThreadBlueprint._vote] "
+                                          f"Can't get votes for thread by slug_or_id = {slug_or_id}", 500)
 
-                entity = self._voteSerializer.load(data)
-                votes = self.__service.vote(entity)
-                if votes is None:
-                    return self._return_error(f"[ThreadBlueprint._vote] "
-                                              f"Can't get votes for thread by slug_or_id = {slug_or_id}", 500)
-
-                thread['votes'] = votes
-                return Response(response=json.dumps(thread), status=200, mimetype='application/json')
-
-            except NoDataFoundError as exp:
-                return self._return_error(f"Can't create thread by request = {request.json}", 404)
+            thread['votes'] = votes
+            return Response(response=json.dumps(thread), status=200, mimetype='application/json')
 
         return blueprint
 
--- src/api/forum/persistence/repositories/thread_repository.py
+++ src/api/forum/persistence/repositories/thread_repository.py
@@ -3,10 +3,7 @@ from typing import Optional, List, Any, Dict
 
 from injector import inject
 
-from forum.persistence.dto.vote_dto import VoteDTO
-from sqlutils import DataContext, Repository, create_one, create_many, return_one, return_many
-
-from forum.persistence.dto.thread_dto import ThreadDTO
+from sqlutils import DataContext,return_one
 
 
 class ThreadRepository(object):
@@ -20,11 +17,7 @@ class ThreadRepository(object):
         response = return_one(data)
         if not response:
             return None
-
-        response.update({
-            'created': response['created'].astimezone().isoformat()
-        })
-
+        response['created'] = response['created'].astimezone().isoformat()
         return response
 
     def get_by_slug(self, slug) -> Optional[Dict[str, Any]]:
@@ -32,12 +25,19 @@ class ThreadRepository(object):
         response = return_one(data)
         if not response:
             return None
-
-        response.update({
-            'created': response['created'].astimezone().isoformat()
-        })
-
+        response['created'] = response['created'].astimezone().isoformat()
         return response
+
+    def get_by_slug_or_id(self, slug_or_id: str) -> Optional[Dict[str, Any]]:
+        try:
+            cast_thread_id = int(slug_or_id)
+            thread = self.get_by_id(cast_thread_id)
+
+        except ValueError:
+            thread_slug = slug_or_id
+            thread = self.get_by_slug(thread_slug)
+
+        return thread
 
     def get_for_forum(self, forum_id: int, **kwargs) -> List[Dict[str, Any]]:
 
@@ -83,8 +83,8 @@ class ThreadRepository(object):
     def clear(self):
         self._context.callproc('clear_threads', [])
 
-    def vote(self, entity: VoteDTO) -> Optional[int]:
-        data = self._context.callproc('add_vote', [entity.user_id, entity.thread_id, entity.vote_value])
+    def vote(self, user_id: int, thread_id: int, voice: int) -> Optional[int]:
+        data = self._context.callproc('add_vote', [user_id, thread_id, voice])
         result_dict = data[0]
         votes = result_dict.get('votes')
         return votes
@@ -107,46 +107,57 @@ class ThreadRepository(object):
         }
 
         if params['created'] is not None:
-            response.update({
-                'created': params['created'].astimezone().isoformat()
-            })
+            response['created'] = params['created'].astimezone().isoformat()
 
         if params['slug'] is not None:
-            response.update({
-                'slug': params['slug']
-            })
+            response['slug'] = params['slug']
 
         return response
 
-    def update_by_slug(self, entity: ThreadDTO) -> Optional[ThreadDTO]:
-        msg = entity.message
-        title = entity.title
+    def update_by_slug(self, slug: str, msg: str = None, title: str = None) -> Optional[Dict[str, Any]]:
 
         data = None
         if msg is not None and title is not None:
-            data = self._context.callproc('update_thread_by_slug_by_msg_title', [entity.slug, msg, title])
+            data = self._context.callproc('update_thread_by_slug_by_msg_title', [slug, msg, title])
 
         elif msg is not None and title is None:
-            data = self._context.callproc('update_thread_by_slug_by_msg', [entity.slug, msg])
+            data = self._context.callproc('update_thread_by_slug_by_msg', [slug, msg])
 
         elif msg is None and title is not None:
-            data = self._context.callproc('update_thread_by_slug_by_title', [entity.slug, title])
+            data = self._context.callproc('update_thread_by_slug_by_title', [slug, title])
 
-        new_entity = create_one(ThreadDTO, data)
-        return new_entity
+        response = return_one(data)
+        if not response:
+            return None
+        response['created'] = response['created'].astimezone().isoformat()
+        return response
 
-    def update_by_uid(self, entity: ThreadDTO) -> Optional[ThreadDTO]:
-        msg = entity.message
-        title = entity.title
+    def update_by_uid(self, uid: int, msg: str = None, title: str = None) -> Optional[Dict[str, Any]]:
 
         data = None
         if msg is not None and title is not None:
-            data = self._context.callproc('update_thread_by_uid_by_msg_title', [entity.uid, msg, title])
+            data = self._context.callproc('update_thread_by_uid_by_msg_title', [uid, msg, title])
 
         elif msg is not None and title is None:
-            data = self._context.callproc('update_thread_by_uid_by_msg', [entity.uid, msg])
+            data = self._context.callproc('update_thread_by_uid_by_msg', [uid, msg])
 
         elif msg is None and title is not None:
-            data = self._context.callproc('update_thread_by_uid_by_title', [entity.uid, title])
+            data = self._context.callproc('update_thread_by_uid_by_title', [uid, title])
 
-        return create_one(ThreadDTO, data)
+        response = return_one(data)
+        if not response:
+            return None
+        response['created'] = response['created'].astimezone().isoformat()
+        return response
+
+    def update_by_slug_or_id(self, slug_or_id: str, msg: str = None, title: str = None):
+        try:
+
+            cast_thread_id = int(slug_or_id)
+            thread = self.update_by_uid(uid=cast_thread_id, msg=msg, title=title)
+
+        except ValueError:
+            thread_slug = slug_or_id
+            thread = self.update_by_slug(slug=thread_slug, msg=msg, title=title)
+
+        return thread
